7ffba44a38cc13a6159e680c7125a9b828a3d0cb
sawan gupta <sawan.gupta92@gmail.com>
2016-04-14 00:31:01 +0530

    move retrieve_searcher_products to concern

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,17 +1,11 @@
 module Spree
   HomeController.class_eval do
 
+    include ProductRetriever
     alias_method :orig_index, :index
 
     def index
       @featured_taxons = Spree::Taxon.where(is_featured: true).includes(:products)
     end
-
-    private
-      def retrieve_searcher_products(taxon_id)
-        build_searcher(taxon: taxon_id, include_images: true).retrieve_products.limit(3)
-      end
-      helper_method :retrieve_searcher_products
-
   end
 end
