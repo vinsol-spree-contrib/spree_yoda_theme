541ba6153134e728c7d35f30d255f900799e3d61
sawan gupta <sawan.gupta92@gmail.com>
2016-04-20 17:09:37 +0530

    display taxons with products

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -22,13 +22,17 @@ module Spree
       return '' if max_level < 1 || root_taxon.leaf?
       content_tag :div, class: 'list-group' do
         taxons = root_taxon.children.map do |taxon|
-          if taxon.products.count > 0
+          if fetch_products_with_product(taxon) > 0
             css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
             link_to(taxon.name, seo_url(taxon), class: css_class) + product_taxons_tree(taxon, current_taxon, max_level - 1)
           end
         end
         safe_join(taxons, "\n")
       end
+    end
+
+    def fetch_products_with_product(taxon)
+      Spree::Classification.where(taxon_id: taxon.self_and_descendants.pluck(:id)).count
     end
   end
 end
