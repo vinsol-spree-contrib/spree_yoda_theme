30e7adb99f89812aff3b12f6ea1962cd01aa7b5b
sawan gupta <sawan.gupta92@gmail.com>
2016-04-26 19:06:39 +0530

    in products index page only show products that belong to any category

--- app/controllers/concerns/spree/products_controller_helper.rb
+++ app/controllers/concerns/spree/products_controller_helper.rb
@@ -4,17 +4,21 @@ module Spree
     SEARCH_RESULTS_PAGINATION_LIMIT = 16
 
     def index
+      if params[:taxon].blank? && Spree::Config[:theme_name] == 'yoda'
+        @taxon = Spree::Taxon.find_by(name: 'Categories')
+        params.merge!(taxon: @taxon.id)
+      end
       super
-      respond_to do |format|
-        format.html do
-          if Spree::Config[:theme_name] == 'yoda'
-            taxon = Spree::Taxon.find_by(name: 'Categories')
-            @products = Spree::Product.where(id: Spree::Classification.where(taxon_id: taxon.self_and_descendants.pluck(:id)).pluck(:product_id).uniq).page(params[:page]).per(SEARCH_RESULTS_PAGINATION_LIMIT)
+      if Spree::Config[:theme_name] == 'yoda'
+        respond_to do |format|
+          format.html do
+            @taxon ||= Spree::Taxon.find_by(name: 'Categories')
+            @products = Spree::Product.where(id: Spree::Classification.where(taxon_id: @taxon.self_and_descendants.pluck(:id)).pluck(:product_id).uniq).page(params[:page]).per(SEARCH_RESULTS_PAGINATION_LIMIT)
           end
-        end
-        format.js do
-          @products = @products.page(params[:page]).per(SEARCH_RESULTS_PAGINATION_LIMIT) if Spree::Config[:theme_name] == 'yoda'
-          render 'index.js'
+          format.js do
+            @products = @products.per(SEARCH_RESULTS_PAGINATION_LIMIT) if Spree::Config[:theme_name] == 'yoda'
+            render 'index.js'
+          end
         end
       end
     end
