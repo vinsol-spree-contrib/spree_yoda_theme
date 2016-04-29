52372c593ed9245597c92f7dc172f0727bfe9e75
sawan gupta <sawan.gupta92@gmail.com>
2016-04-29 16:42:20 +0530

    On empty cart page show products that belongs to "Cateories"

--- app/controllers/concerns/spree/orders_controller_helper.rb
+++ app/controllers/concerns/spree/orders_controller_helper.rb
@@ -5,7 +5,9 @@ module Spree
 
     def edit
       super
-      @products = Spree::Product.limit(PRODUCTS_LIMIT) if @order.line_items.empty?
+      @taxon ||= Spree::Taxon.find_by(name: 'Categories')
+      classifications_id = Spree::Classification.where(taxon_id: @taxon.self_and_descendants.pluck(:id)).pluck(:product_id)
+      @products = Spree::Product.where(id: classifications_id).limit(PRODUCTS_LIMIT) if @order.line_items.empty?
     end
   end
 end
